d9785b94f390b105684b9cfaf01aaf37f7f93880
migueldossantos011@gmail.com <migueldossantos011@gmail.com>
2020-06-05 14:04:58 -0300

    Adicionando programas cobol

---
+++ PROGCOB03.cob
@@ -0,0 +1,22 @@
+       IDENTIFICATION DIVISION.
+
+       PROGRAM-ID. PROGCOB03.
+      ******************************************
+      * AREA DE COMENTARIOS - REMARKS
+      * AUTHOR - MIGUEL DOS SAMTOS RIBEIRO MISANRI
+      * OBJETIVO - IMPRIMIR A DATA DO SISTEMA
+      * UTILIZAR AS VARIAVEIS COM NIVEL 01..02..(ESTRUTURADA)
+      *DATA - 05/06/2020
+      ******************************************
+       ENVIRONMENT DIVISION.
+
+       DATA DIVISION.
+       WORKING-STORAGE SECTION.
+       01 WRK-DATA.
+              02 WRK-ANO PIC 9(04) VALUES ZEROS.
+              02 WRK-MES PIC 9(02) VALUES ZEROS.
+              02 WRK-DIA PIC 9(02) VALUES ZEROS.
+       PROCEDURE DIVISION.
+              ACCEPT WRK-DATA FROM DATE YYYYMMDD.
+              DISPLAY 'DATA: ' WRK-DIA '/' WRK-MES '/' WRK-ANO.
+              STOP RUN.
---
+++ PROGCOB04.cbl
@@ -0,0 +1,28 @@
+       IDENTIFICATION DIVISION.
+
+       PROGRAM-ID. PROGCOB03.
+      ******************************************
+      * AREA DE COMENTARIOS - REMARKS
+      * AUTHOR - MIGUEL DOS SAMTOS RIBEIRO MISANRI
+      * OBJETIVO - IMPRIMIR NOME E SALARIO
+      * IMPRIMIR FORMATADO
+      *DATA - 05/06/2020
+      ******************************************
+       ENVIRONMENT DIVISION.
+       CONFIGURATION SECTION.
+       SPECIAL-NAMES.
+           DECIMAL-POINT IS COMMA.
+       DATA DIVISION.
+       WORKING-STORAGE SECTION.
+       77 WRK-NOME          PIC X(20)       VALUE SPACES.
+       77 WRK-SALARIO       PIC 9(06)V99    VALUE ZEROS.
+       77 WRK-SALARIO-ED    PIC $ZZZ.ZZ9,99 VALUE ZEROS.
+       PROCEDURE DIVISION.
+           ACCEPT WRK-NOME         FROM CONSOLE.
+           ACCEPT WRK-SALARIO      FROM CONSOLE.
+
+      *************** MOSTRA DADOS *************
+           DISPLAY 'NOME 'WRK-NOME.
+           MOVE WRK-SALARIO TO WRK-SALARIO-ED.
+           DISPLAY   'SALARIO ' WRK-SALARIO-ED.
+           STOP RUN.
