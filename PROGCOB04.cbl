4df449028373021b5961075ed6656cf39453c23d
migueldossantos011@gmail.com <migueldossantos011@gmail.com>
2020-06-11 21:54:51 -0300

    Adicionando mais arquivos ao repositório

--- PROGCOB04.cbl
+++ PROGCOB04.cbl
@@ -14,15 +14,16 @@
            DECIMAL-POINT IS COMMA.
        DATA DIVISION.
        WORKING-STORAGE SECTION.
-       77 WRK-NOME          PIC X(20)       VALUE SPACES.
-       77 WRK-SALARIO       PIC 9(06)V99    VALUE ZEROS.
+       77 WRK-NOME          PIC X(20) VALUE SPACES.
+       77 WRK-SALARIO       PIC 9(06)V99 VALUE ZEROS.
        77 WRK-SALARIO-ED    PIC $ZZZ.ZZ9,99 VALUE ZEROS.
        PROCEDURE DIVISION.
-           ACCEPT WRK-NOME         FROM CONSOLE.
-           ACCEPT WRK-SALARIO      FROM CONSOLE.
+           ACCEPT    WRK-NOME      FROM CONSOLE.
+           ACCEPT    WRK-SALARIO   FROM CONSOLE.
 
-      *************** MOSTRA DADOS *************
-           DISPLAY 'NOME 'WRK-NOME.
-           MOVE WRK-SALARIO TO WRK-SALARIO-ED.
-           DISPLAY   'SALARIO ' WRK-SALARIO-ED.
+      **************MOSTRA DADOS************************
+
+           DISPLAY 'NOME: '        WRK-NOME.
+           MOVE WRK-SALARIO        TO WRK-SALARIO-ED.
+           DISPLAY 'SALARIO: '     WRK-SALARIO-ED.
            STOP RUN.
